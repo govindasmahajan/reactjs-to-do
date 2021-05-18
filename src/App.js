b549c785d7adf66046e8d0bdf1c496bd26e08290
Govinda Mahajan <govinda.mahajan@infostretch.com>
2021-05-18 16:40:49 +0530

    multiple componets added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
 import Todo from "./components/Todo";
 
 function App() {
   return (
     <div>
       <h1>My To Do</h1>
-      <Todo text='Learn React' name='Started'/>
-      <Todo text='Learn React Native' name='Not yet'/>
+      <Todo text='Learn React' name='Delete' />
+      <Todo text='Learn React Native' name='Delete' />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,13 @@
 function Todo(props) {
+    function deleteHandler() {
+        console.log('Clicked here!!!');
+        console.log(props.text);
+    }
     return (
         <div className='card'>
             <h2>{props.text}</h2>
             <div className='actions'>
-                <button className='btn'>{props.name}</button>
+                <button className='btn' onClick={deleteHandler}>{props.name}</button>
             </div>
         </div>
     )
